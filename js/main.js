3adefafcab741171b3533e9f617fc78009bce665
Cursi <razvan.diib@yahoo.com>
2017-08-02 22:36:55 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//---------------------------------
+
+	
+
+//---------------------------------
+
+$( document ).ready(function() 
+{	
+	    var container = document.getElementById("binary");
+        var str = container.innerHTML;
+        var newText = "";
+        for (var i = 0; i < str.length; i++) 
+		{
+            if (str[i] == 'A')  newText+= str.charAt(i).fontcolor("#3399ff");
+            else newText+= str.charAt(i).fontcolor("#ff704d");
+        }
+        container.innerHTML = newText; 
+});
+
